f38c015c904d872fe320c4570011a545f3a55425
Ezequiel <ezequielmatias.af@gmail.com>
2023-02-12 13:39:40 +0100

    Curso 1:26:00 Crea Usuario.java y busca el usuario por ID, sin Base de Datos aún, manual.

--- src/main/java/com/s2daw/demo/controllers/UsuarioController.java
+++ src/main/java/com/s2daw/demo/controllers/UsuarioController.java
@@ -1,5 +1,7 @@
 package com.s2daw.demo.controllers;
 
+import com.s2daw.demo.models.Usuario;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,8 +10,49 @@ import java.util.List;
 @RestController
 public class UsuarioController {
 
-    @RequestMapping(value="prueba")
-    public List<String> prueba(){
-        return List.of("prueba","prueba2","prueba3");
+    @RequestMapping(value="usuario/{id}")
+    public Usuario getUsuario(@PathVariable Long id){
+        Usuario usuario = new Usuario();
+        usuario.setId(id);
+        usuario.setNombre("Ezequiel");
+        usuario.setApellido("Arean");
+        usuario.setEmail("ezequielmatias.af@gmail.com");
+        usuario.setTelefono("722126018");
+        usuario.setPassword("ArgentinaCampeona");
+
+        return usuario;
+    }
+    @RequestMapping(value="usuario848")
+    public Usuario editar(){
+        Usuario usuario = new Usuario();
+        usuario.setNombre("Ezequiel");
+        usuario.setApellido("Arean");
+        usuario.setEmail("ezequielmatias.af@gmail.com");
+        usuario.setTelefono("722126018");
+        usuario.setPassword("ArgentinaCampeona");
+
+        return usuario;
+    }
+    @RequestMapping(value="usuario7554")
+    public Usuario eliminar(){
+        Usuario usuario = new Usuario();
+        usuario.setNombre("Ezequiel");
+        usuario.setApellido("Arean");
+        usuario.setEmail("ezequielmatias.af@gmail.com");
+        usuario.setTelefono("722126018");
+        usuario.setPassword("ArgentinaCampeona");
+
+        return usuario;
+    }
+    @RequestMapping(value="usuario7848")
+    public Usuario buscar(){
+        Usuario usuario = new Usuario();
+        usuario.setNombre("Ezequiel");
+        usuario.setApellido("Arean");
+        usuario.setEmail("ezequielmatias.af@gmail.com");
+        usuario.setTelefono("722126018");
+        usuario.setPassword("ArgentinaCampeona");
+
+        return usuario;
     }
 }

6c4777ff1afeae74ad508cfe3996242cc8bf7400
Ezequiel <ezequielmatias.af@gmail.com>
2023-02-14 14:02:13 +0100

    Hasta 2:25:00

--- src/main/java/com/s2daw/demo/controllers/UsuarioController.java
+++ src/main/java/com/s2daw/demo/controllers/UsuarioController.java
@@ -1,6 +1,8 @@
 package com.s2daw.demo.controllers;
 
+import com.s2daw.demo.dao.UsuarioDao;
 import com.s2daw.demo.models.Usuario;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,43 +13,20 @@ import java.util.List;
 @RestController
 public class UsuarioController {
 
+    @Autowired
+    private UsuarioDao usuarioDao;
     @RequestMapping(value="usuario/{id}")
     public Usuario getUsuario(@PathVariable Long id){
         Usuario usuario=new Usuario();
         usuario.setId(id);
-        usuario.setNombre("Ezequiel");
-        usuario.setApellido("Arean");
-        usuario.setEmail("ezequielmatias.af@gmail.com");
-        usuario.setTelefono("722126018");
+        usuario.setNombre("Lucas");
+        usuario.setApellidos("Moy");
+        usuario.setEmail("lucasmoy@hotmail.com");
+        usuario.setTelefono("234234234");
         return usuario;
     }
     @RequestMapping(value="usuarios")
     public List<Usuario> getUsuarios(){
-        List<Usuario> usuarios=new ArrayList<>();
-        Usuario usuario=new Usuario();
-        usuario.setId(123L);
-        usuario.setNombre("Ezequiel");
-        usuario.setApellido("Matías");
-        usuario.setEmail("ezequielmatias.af@@gmail.com");
-        usuario.setTelefono("722126018");
-        usuarios.add(usuario);
-
-        usuario=new Usuario();
-        usuario.setId(268L);
-        usuario.setNombre("Oscar");
-        usuario.setApellido("Arean");
-        usuario.setEmail("oscar.a@gmail.com");
-        usuario.setTelefono("45965238");
-        usuarios.add(usuario);
-
-        usuario=new Usuario();
-        usuario.setId(3L);
-        usuario.setNombre("Sebast");
-        usuario.setApellido("Fernández");
-        usuario.setEmail("sebast.f@gmail.com");
-        usuario.setTelefono("456921238");
-        usuarios.add(usuario);
-
-        return usuarios;
+        return usuarioDao.getUsuarios();
     }
 }

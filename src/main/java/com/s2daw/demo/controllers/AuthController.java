53fdc7efccfb5d17a44948669e275aa40beedd81
Ezequiel <ezequielmatias.af@gmail.com>
2023-02-15 11:44:57 +0100

    Desde 2:40:00 a 3:07:00, creamos la función para registrar usuarios

---
+++ src/main/java/com/s2daw/demo/controllers/AuthController.java
@@ -0,0 +1,2 @@
+package com.s2daw.demo.controllers;public class AuthController {
+}
--- src/main/java/com/s2daw/demo/dao/UsuarioDao.java
+++ src/main/java/com/s2daw/demo/dao/UsuarioDao.java
@@ -7,6 +7,7 @@ import java.util.List;
 public interface UsuarioDao {
     List<Usuario> getUsuarios();
 
-
     void eliminarUsuario(Long id);
+
+    void registraUsuario(Usuario usuario);
 }
